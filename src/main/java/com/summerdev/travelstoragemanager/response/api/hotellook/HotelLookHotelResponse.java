80e614eac108e5edd5c8050288c140706e070d4b
rdnt42 <radiant42@yandex.ru>
2022-05-25 23:23:42 +0300

    #69 added lombok constructors for json mapping

--- src/main/java/com/summerdev/travelstoragemanager/response/api/hotellook/HotelLookHotelResponse.java
+++ src/main/java/com/summerdev/travelstoragemanager/response/api/hotellook/HotelLookHotelResponse.java
@@ -1,10 +1,14 @@
 package com.summerdev.travelstoragemanager.response.api.hotellook;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
+@NoArgsConstructor
+@AllArgsConstructor
 @Data
 @Builder
 public class HotelLookHotelResponse {
--- src/main/java/com/summerdev/travelstoragemanager/response/api/tutu/TutuTrainsResponse.java
+++ src/main/java/com/summerdev/travelstoragemanager/response/api/tutu/TutuTrainsResponse.java
@@ -1,12 +1,16 @@
 package com.summerdev.travelstoragemanager.response.api.tutu;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
+@NoArgsConstructor
+@AllArgsConstructor
 @Data
 public class TutuTrainsResponse {
     List<TutuTripItemResponse> trips = new ArrayList<>();
